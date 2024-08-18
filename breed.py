cc349aeeab204ba66af72c19e7a7922902479ae3
macovskyy <macpfords@gmail.com>
2024-08-18 16:42:03 +0300

    deploying

--- breed.py
+++ breed.py
@@ -3,33 +3,37 @@ import json
 from tensorflow.keras.preprocessing import image
 import numpy as np
 
+# Завантаження збереженої моделі
 model = tf.keras.models.load_model('cat_breed_classifier.keras')
 
+# Завантаження мапи класів з файлу JSON
 with open('class_indices.json', 'r') as f:
     class_indices = json.load(f)
 
+# Перетворення мапи класів у зворотну форму (індекс до назви)
 class_indices = {v: k for k, v in class_indices.items()}
 
 
-
 def prepare_image(img_path):
-    # Измените размер, если требуется
+    # Завантаження зображення та зміна його розміру
     img = image.load_img(img_path, target_size=(128, 128))
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)  # Добавление оси для пакета
-    img_array /= 255.0  # Нормализация
+    img_array = image.img_to_array(img)  # Перетворення зображення у масив
+    img_array = np.expand_dims(img_array, axis=0)  # Додавання осі для пакета
+    img_array /= 255.0  # Нормалізація значень пікселів
     return img_array
 
 
-
 def predict_breed(img_path):
-    img = prepare_image(img_path)
-    predictions = model.predict(img)
+    img = prepare_image(img_path)  # Підготовка зображення
+    predictions = model.predict(img)  # Прогнозування класу
+    # Отримання індексу класу з найбільшою ймовірністю
     predicted_class_index = np.argmax(predictions)
+    # Отримання назви класу за індексом
     predicted_class = class_indices[predicted_class_index]
     return predicted_class
 
 
-img_path = input('path:' ) 
-breed = predict_breed(img_path)
-print(f'The predicted breed is): {breed}')
+# Введення шляху до зображення користувачем
+img_path = input('path: ')
+breed = predict_breed(img_path)  # Прогнозування породи
+print(f'The predicted breed is: {breed}')  # Виведення результату
--- breed_education.py
+++ breed_education.py
@@ -6,12 +6,13 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
 from tensorflow.keras.optimizers import Adam
 
-# Пути к данным
+# Шлях до даних
 base_dir = 'data'
 
-# Data generators
+# Генератори даних
 train_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
 
+# Генератор для навчальної вибірки
 train_generator = train_datagen.flow_from_directory(
     base_dir,
     target_size=(128, 128),
@@ -20,6 +21,7 @@ train_generator = train_datagen.flow_from_directory(
     subset='training'
 )
 
+# Генератор для валідаційної вибірки
 validation_generator = train_datagen.flow_from_directory(
     base_dir,
     target_size=(128, 128),
@@ -28,31 +30,36 @@ validation_generator = train_datagen.flow_from_directory(
     subset='validation'
 )
 
-# Модель с MobileNetV2
+# Модель на основі MobileNetV2
 base_model = MobileNetV2(
     weights='imagenet', include_top=False, input_shape=(128, 128, 3))
 x = base_model.output
-x = GlobalAveragePooling2D()(x)
-x = Dense(1024, activation='relu')(x)
+x = GlobalAveragePooling2D()(x)  # Глобальне середнє пулінгування
+x = Dense(1024, activation='relu')(x)  # Повнозв'язковий шар з активацією ReLU
 predictions = Dense(len(train_generator.class_indices),
+                    # Повнозв'язковий шар для класифікації
                     activation='softmax')(x)
 
 model = Model(inputs=base_model.input, outputs=predictions)
 
+# Замороження ваг базової моделі (MobileNetV2) для навчання тільки нових шарів
 for layer in base_model.layers:
     layer.trainable = False
 
+# Компіляція моделі
 model.compile(optimizer=Adam(), loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-# Обучение модели
+# Навчання моделі
 model.fit(
     train_generator,
     epochs=10,
     validation_data=validation_generator
 )
 
+# Збереження моделі
 model.save('cat_breed_classifier.keras')
 
+# Збереження класів у файл JSON
 with open('class_indices.json', 'w') as f:
     json.dump(train_generator.class_indices, f)
